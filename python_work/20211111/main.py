ef1869a87ba0fd16b556594ae4e9144448e9d999
DongJoonLeeDJ <ldjgood12@gmail.com>
2021-11-11 15:52:57 +0900

    20211111

--- python_work/20211111/main.py
+++ python_work/20211111/main.py
@@ -1,10 +1,12 @@
 import sys
 from PyQt5.QtWidgets import *
+from myExcel import MExcel
 
 class MyApp(QWidget):
     def __init__(self):
         super().__init__()
         self.initUi()
+        self.m = MExcel()
 
     ''' 화면을 만드는 내용...'''
     def initUi(self):
@@ -15,15 +17,17 @@ class MyApp(QWidget):
         label_3 = QLabel("입력3", self);
         label_3.move(20, 95)
 
-        lineEdit1 = QLineEdit("aaa",self)
-        lineEdit1.setFixedSize(65,20)
-        lineEdit1.move(70,32)
-        lineEdit2 = QLineEdit("bbb", self)
-        lineEdit2.setFixedSize(65, 20)
-        lineEdit2.move(70, 64)
-        lineEdit3 = QLineEdit("ccc", self)
-        lineEdit3.setFixedSize(65, 20)
-        lineEdit3.move(70, 92)
+        self.lineEdit1 = QLineEdit("aaa",self)
+        self.lineEdit1.setFixedSize(65,20)
+        self.lineEdit1.move(70,32)
+
+        self.lineEdit2 = QLineEdit("bbb", self)
+        self.lineEdit2.setFixedSize(65, 20)
+        self.lineEdit2.move(70, 64)
+
+        self.lineEdit3 = QLineEdit("ccc", self)
+        self.lineEdit3.setFixedSize(65, 20)
+        self.lineEdit3.move(70, 92)
 
         btnsave = QPushButton("저장",self)
         btnsave.move(150,30)
@@ -45,13 +49,17 @@ class MyApp(QWidget):
 
     ''' 엑셀에 있는 내용을 save'''
     def btnsavefunc(self):
-        print("btnsave")
+        a = self.lineEdit1.text()
+        b = self.lineEdit2.text()
+        c = self.lineEdit3.text()
+
+        self.m.save(a,b,c)
 
     def btnloadfunc(self):
-        print("btnload")
+        self.m.load()
 
     def btncreatefunc(self):
-        print("btncreate")
+        self.m.create()
 
 
 if __name__ == '__main__':
--- python_work/20211111/myExcel.py
+++ python_work/20211111/myExcel.py
@@ -1,9 +1,40 @@
+from openpyxl import *
+
+'''
+    Workbook() 엑셀 파일이 없을때 생성
+    load_workbook('a.xlsx') 엑셀파일이 있을때 불러오는것
+
+    wb.save('a.xlsx')
+    wb.close()
+'''
+
 class MExcel:
     def __init__(self):
         pass
-    def save(self):
-        pass
+
+    def save(self,a,b,c):
+        wb = load_workbook('mexcel.xlsx')
+        ws = wb.active
+        ws.append([a,b,c])
+        wb.save('mexcel.xlsx')
+        wb.close()
+
     def load(self):
-        pass
+        wb = load_workbook('mexcel.xlsx')
+        ws = wb.active
+
+        # iter_rows 행을 기준으로 반복하는 함수
+        # iter_cols 열을 기준으로 반복하는 함수
+        for row in ws.iter_rows():
+            for cell in row:  # 11 22 33
+                print(cell.value, end=' ')
+            print()
+
+        wb.save('mexcel.xlsx')
+        wb.close()
+
     def create(self):
-        pass
+        wb = Workbook()
+        wb.save('mexcel.xlsx')
+        wb.close()
+
