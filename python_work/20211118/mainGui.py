92fafec1511b4991d2da92d66dc030b9a334d482
DongJoonLeeDJ <ldjgood12@gmail.com>
2021-11-18 15:30:59 +0900

    20211118

--- python_work/20211118/mainGui.py
+++ python_work/20211118/mainGui.py
@@ -6,20 +6,49 @@ from scoreExcel import ScoreExcel
 class MyApp(QWidget):
     def __init__(self):
         super().__init__()
-        self.initUi()   #화면생성
-        self.initEvent() #시그널생성 이벤트함수
+
+        self.se = ScoreExcel()
+        self.rowIndex = 0
+
+        self.initUi()  # 화면생성
+        self.initEvent()  # 시그널생성 이벤트함수
+
+        self.loadfile()
+
+        # 조회... 가져오기...# checkbox....
+    def loadfile(self):
+        rows = self.se.loadrow()
+
 
     def append(self):
         try:
-            ScoreExcel().appendrow(100,70,50)
+            kor = self.kor_edit.text()
+            eng = self.eng_edit.text()
+            math = self.math_edit.text()
+            # 100 + 50 + 80 = 1005080
+            tot = int(kor) + int(eng) + int(math)
+            # 76.666666 *100 -> 7666.66666
+            # int (7666.6666) -> 7666
+            # 7666 /100 -> 76.66
+            avg = int(tot / 3 * 100) / 100
+
+            self.qtable.setItem(self.rowIndex, 0, QTableWidgetItem(kor))
+            self.qtable.setItem(self.rowIndex, 1, QTableWidgetItem(eng))
+            self.qtable.setItem(self.rowIndex, 2, QTableWidgetItem(math))
+            self.qtable.setItem(self.rowIndex, 3, QTableWidgetItem(str(tot)))
+            self.qtable.setItem(self.rowIndex, 4, QTableWidgetItem(str(avg)))
+
+            self.se.appendrow(kor, eng, math, str(tot), str(avg))
+
+            self.rowIndex += 1
         except Exception as e:
             print(e)
 
     def delete(self):
-        ScoreExcel().appendrow()
+        self.se.appendrow()
 
     def create(self):
-        ScoreExcel().appendrow()
+        self.se.createfile()
 
     def initEvent(self):
         self.add_btn.clicked.connect(self.append)
@@ -33,11 +62,11 @@ class MyApp(QWidget):
         self.qtable.setColumnCount(5)
 
         self.qtable.setHorizontalHeaderLabels(['국어', '영어', '수학', '총점', '평균'])
-        self.qtable.horizontalHeader()\
+        self.qtable.horizontalHeader() \
             .setSectionResizeMode(QHeaderView.Stretch)
 
-        self.qtable.move(60,120)
-        self.qtable.setFixedSize(500,250)
+        self.qtable.move(60, 120)
+        self.qtable.setFixedSize(500, 250)
 
         kor_label = QLabel("국어", self)
         kor_label.move(50, 40)
@@ -46,15 +75,15 @@ class MyApp(QWidget):
         math_label = QLabel("수학", self)
         math_label.move(50, 90)
 
-        self.kor_edit = QLineEdit("100",self)
+        self.kor_edit = QLineEdit("100", self)
         self.kor_edit.move(100, 37)
         self.eng_edit = QLineEdit("80", self)
         self.eng_edit.move(100, 62)
         self.math_edit = QLineEdit("50", self)
         self.math_edit.move(100, 87)
 
-        self.add_btn = QPushButton("추가",self)
-        self.add_btn.move(500,30)
+        self.add_btn = QPushButton("추가", self)
+        self.add_btn.move(500, 30)
         self.del_btn = QPushButton("삭제", self)
         self.del_btn.move(500, 55)
         self.create_btn = QPushButton("파일생성", self)
--- python_work/20211118/scoreExcel.py
+++ python_work/20211118/scoreExcel.py
@@ -1,3 +1,4 @@
+from openpyxl import load_workbook, Workbook
 
 
 class ScoreExcel:
@@ -5,6 +6,40 @@ class ScoreExcel:
     def __init__(self):
         print("생성자")
 
+    def loadrow(self):
+        print("일로오나")
+        rows = []
+        try:
+            wb = load_workbook('score.xlsx')
+
+            ws = wb.active
+
+            for row in ws.iter_rows(min_row=2):
+                rows.append([row[0].value, row[1].value,
+                             row[2].value, row[3].value,
+                             row[4].value])
+
+            print(rows)
+
+            wb.close()
+            return rows
+        except Exception as e:
+            print(e)
+
     # 추가..
-    def appendrow(self, kor, eng, math):
-        print("추가해야됨")
+    def appendrow(self, kor, eng, math, tot, avg):
+        wb = load_workbook('score.xlsx')
+        ws = wb.active
+
+        ws.append([kor, eng, math, tot, avg])
+        wb.save("score.xlsx")
+        wb.close()
+
+    def createfile(self):
+        wb = Workbook()
+        ws = wb.active
+
+        ws.append(['국어', '영어', '수학', '총점', '평균'])
+
+        wb.save("score.xlsx")
+        wb.close()
