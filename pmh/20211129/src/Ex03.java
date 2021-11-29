b14730fe68f86749ee410ca2012d6bc8c8ec88cd
DongJoonLeeDJ <ldjgood12@gmail.com>
2021-11-29 11:07:16 +0900

    20211129

--- pmh/20211129/src/Ex03.java
+++ pmh/20211129/src/Ex03.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 class Row{
     private String name;
@@ -7,12 +8,28 @@ class Row{
     private int eng;
     private int math;
     private int total;
+
     public Row(String name, int kor, int eng, int math) {
         this.name = name;
         this.kor = kor;
         this.eng = eng;
         this.math = math;
         this.total = kor + eng + math;
+    }
+
+//    @Override
+//    public boolean equals(Object o) {
+//        Row row = (Row) o;
+//        return this.kor == row.kor
+//                && this.eng == row.eng
+//                && this.math == row.math
+//                && this.total == row.total
+//                && Objects.equals(this.name, row.name);
+//    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, kor, eng, math, total);
     }
 
     @Override
@@ -33,7 +50,6 @@ public class Ex03 {
         al.add(new Row("aa",100,80,70));
         al.add(new Row("bbb",90,70,60));
         al.add(new Row("cccc",70,90,80));
-
 
         // List<Row> cpal = new ArrayList<>();
         // for total al.. comparable... 상속받아서... 인터페이스..
--- python_work/pyautogui/20211129/ex01.py
+++ python_work/pyautogui/20211129/ex01.py
@@ -1,7 +1,16 @@
 
-a = [1,2,3,4]
+# a = [1,2,3,4]
+#
+# a.append('문자열')
+# a.append(55)
+#
+# print(a)
 
-a.append('문자열')
-a.append(55)
+b = dict(a=11,b=22,c=33)
 
-print(a)
+print(b)
+print(b['a'])
+
+
+for temp in b.items():
+    print(temp)
