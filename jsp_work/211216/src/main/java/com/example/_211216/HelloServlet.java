b66eb9bceb5541955e7c033dc905d74f32109e00
DongJoonLeeDJ <ldjgood12@gmail.com>
2021-12-16 11:58:44 +0900

    211216
    211216

---
+++ jsp_work/211216/src/main/java/com/example/_211216/HelloServlet.java
@@ -0,0 +1,28 @@
+package com.example._211216;
+
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    // 생성자 처럼 자동으로 호출되는 메서드..
+    public void init() {
+        message = "Hello World!";
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    public void destroy() {
+    }
+}
---
+++ jsp_work/211216/src/main/webapp/index.jsp
@@ -0,0 +1,37 @@
+<%@ page contentType="text/html; charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>JSP - Hello World</title>
+</head>
+<body>
+<!--
+    file
+    a.jsp -> a_jsp a_jsp.class
+    a.java -> a.class
+    -> extends HttpServlet 상속받아서 하는 방법...
+ -->
+<%!
+    // 메서드를 정의 할수가 있다....
+    public void doA(){
+        System.out.println("test");
+    }
+%>
+<%
+    doA();
+    doA();
+%>
+<h1>
+<%= "Hello World!" %>
+<% out.print("Hello World!"); %>
+</h1>
+<br/>
+<a href="hello-servlet">Hello Servlet</a><br>
+<a href="MyServlet">MyServlet</a>
+한글나오냐..
+<form action="submit.jsp" method="get">
+    <input type="text" name="aa" value="han">
+    <input type="submit" value="전송">
+</form>
+</body>
+</html>
