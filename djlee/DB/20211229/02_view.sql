cb7cb99e8f8ecc6f98e6e90765d27a6c8a2ee73e
DongJoonLeeDJ <ldjgood12@gmail.com>
2021-12-29 11:16:51 +0900

    view 이용해서 중첩질의 코드 심플하게 함

--- djlee/DB/20211229/02_view.sql
+++ djlee/DB/20211229/02_view.sql
@@ -19,3 +19,10 @@ select * from expensive_book; -- 10000원을 넘는 책만 출력됨
 --아래 쿼리를 실행한다.
 grant create view to c##scott;
 
+
+select orderid, aa.bookname, customer.name, saleprice from 
+aa join customer on 
+aa.custid = customer.custid order by orderid;
+
+create view aa as select orderid, book.bookname, orders.saleprice, orders.custid from 
+orders join book on orders.bookid=book.bookid;
