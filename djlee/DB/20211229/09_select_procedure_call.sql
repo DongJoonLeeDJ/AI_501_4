2db333dbb6270c72bc542e3a24bf50b4136ffb46
DongJoonLeeDJ <ldjgood12@gmail.com>
2021-12-29 12:38:07 +0900

    crud 프로시저 완료

--- djlee/DB/20211229/09_select_procedure_call.sql
+++ djlee/DB/20211229/09_select_procedure_call.sql
@@ -8,3 +8,8 @@ print o_cursor
 --o_cursor에 있는 것들 print함
 
 --세 줄 다 드래그해서 ctrl enter해서 실행해줘야 함
+
+--실험용 데이터 추가
+
+exec add_person(10,'이동준','010-1111-9999');
+exec add_person(7,'이동준','010-2222-9999');
