0bbc08b1208f22cbf4f0616f8b42b2af9ed0d8fb
DongJoonLeeDJ <ldjgood12@gmail.com>
2022-01-03 15:23:18 +0900

    0103
    0103

--- python_work/alone_ml_dl/chap3/ex04.py
+++ python_work/alone_ml_dl/chap3/ex04.py
@@ -3,6 +3,14 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import PolynomialFeatures
+
+# 1,2,3 2,6,3 -> 10
+# 4,5,6 20,30,24 -> 20
+
+# scale 비율.. 배율.. x좌표... 0~45 y좌표.. 0 ~1000좌표 표준변차..
+
 df = pd.read_csv('https://bit.ly/perch_csv_data')
 perch_full = df.to_numpy()
 
@@ -37,3 +45,31 @@ print('훈련예측 점수', score)
 pred = lr.predict([[36, 10.61, 6.74]])
 print('예측되는값', pred, '실제값', train_target[4])
 
+'''
+    degree 하이퍼 파라메타...
+    1,2,3, 항개수..변수개수..특성개수 
+    ->
+    항이 많으면 많을수록 학습이 잘된다..
+    
+    항이 너무 많으면 학습했을때 학습 데이터로는 정확한 값이 나오지만
+    테스트 데이터로는 예측값을 알 수 없다..
+'''
+pf = PolynomialFeatures(degree=2,include_bias=False)
+pf.fit(train_input)
+
+train_poly = pf.transform(train_input)
+test_poly = pf.transform(test_input)
+# print(pf.get_feature_names())
+# print(train_input[1])
+# print(train_poly[1])
+
+lr = LinearRegression()
+lr.fit(train_poly,train_target)
+
+poly_score = lr.score(test_poly,test_target)
+# print('9개의 항으로 학습 했을 때 학습기 점수',poly_score)
+
+mydata = pf.transform([[36, 10.61, 6.74]])
+# print(mydata)
+pred = lr.predict(mydata)
+# print('pred',pred)
