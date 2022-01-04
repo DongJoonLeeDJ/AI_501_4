5c2891f1397526afb74bfc01141356cf1015d368
DongJoonLeeDJ <ldjgood12@gmail.com>
2022-01-04 13:48:08 +0900

    씨샵 시작!

---
+++ djlee/cSharp/MyHelloCSharp/MyHelloCSharp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyHelloCSharp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //cw 입력하고 tab 키 두 번 누름
+            Console.WriteLine("안녕");
+        }
+    }
+}
