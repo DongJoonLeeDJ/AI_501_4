f5248faa21931240faeae2cb988430d37e54e0e9
DongJoonLeeDJ <ldjgood12@gmail.com>
2022-01-05 15:36:39 +0900

    Update Program.cs

--- djlee/cSharp/20220105/MyCSharp_20220105/MyCSharp_20220105_5/Program.cs
+++ djlee/cSharp/20220105/MyCSharp_20220105/MyCSharp_20220105_5/Program.cs
@@ -23,7 +23,7 @@ namespace MyCSharp_20220105_5
                 }
             }
 
-            if(false)
+            if (false)
             {
                 Console.WriteLine("goto문으로 인한 이동");
                 int count = 0;
@@ -48,7 +48,7 @@ namespace MyCSharp_20220105_5
             int sum = 0;
             for (int i = 1; i <= 100; i++)
                 sum += i;
-            Console.WriteLine("sum="+sum);
+            Console.WriteLine("sum=" + sum);
 
             //반복문에서 나오는 키워드 : break와 continue
             sum = 0;
@@ -60,11 +60,11 @@ namespace MyCSharp_20220105_5
                     break; //반복문 종료
                 mycount++;
             }
-            Console.WriteLine("sum="+sum);
+            Console.WriteLine("sum=" + sum);
 
             //짝수인 경우만 더해보자.(1부터 10까지)
             sum = 0;
-            for(int i = 1; i<=10; i++)
+            for (int i = 1; i <= 10; i++)
             {
                 if (i % 2 != 0) //홀수인 경우
                     continue; //continue 키워드
@@ -72,7 +72,7 @@ namespace MyCSharp_20220105_5
                               //다음단계로 넘어가버림
                 sum += i; //홀수가 아닌 경우 즉 짝수인 경우엔 더한다.
             }
-            Console.WriteLine("sum(짝수)="+sum);
+            Console.WriteLine("sum(짝수)=" + sum);
 
 
             //foreach
@@ -85,7 +85,7 @@ namespace MyCSharp_20220105_5
             myarr[4] = 34;
             //myarr에 있는 값에 접근해서, 값들 출력하고'
             //여기 있는 값들을 더해서 새로운 숫자를 만들고 싶다면?
-            
+
             //foreach : 배열이나 List에 있는 각각의 값들에 접근해주고,
             //배열의 길이만큼 반복해주는 반복문
             sum = 0;
@@ -96,18 +96,18 @@ namespace MyCSharp_20220105_5
             var mytest = 100; //mytest라는 변수는 100이 들어갔으므로 int타입
             foreach (var item in myarr)
             {
-                sum +=item;
-                Console.WriteLine("item = "+item);
+                sum += item;
+                Console.WriteLine("item = " + item);
             }
-            Console.WriteLine("sum="+sum);
+            Console.WriteLine("sum=" + sum);
             sum = 0;
             Console.WriteLine("for문 활용");
-            for(int i = 0; i < myarr.Length; i++)
+            for (int i = 0; i < myarr.Length; i++)
             {
                 sum += myarr[i];
                 Console.WriteLine($"myarr[{i}]={myarr[i]}");
             }
-            Console.WriteLine("sum="+sum);
+            Console.WriteLine("sum=" + sum);
 
             foreach (var item in myarr)
             {
@@ -126,9 +126,45 @@ namespace MyCSharp_20220105_5
 
             }
 
+            //이렇게 쓰면 가독성 떨어짐
+            if (testcount > 100) Console.WriteLine("testcount는 100넘음");
+
             Console.WriteLine("안녕히 가세요.");
-            mytest:
-                Console.WriteLine("여기로 순간이동했어요");
+        mytest:
+            Console.WriteLine("여기로 순간이동했어요");
+
+
+            Console.WriteLine("비파괴적 메서드(원본 변경 안 함)");
+            string tomato = "My Tomato";
+            Console.WriteLine(tomato.ToLower());
+            Console.WriteLine(tomato.ToUpper());
+            Console.WriteLine(tomato);
+            string sentense = "안녕 하세요. 이동준 입니다.";
+            //Split - 문자열 자르기
+            //공백으로 잘라보기
+            string[] stringarray = sentense.Split(' ');
+            foreach (var item in stringarray)
+            {
+                Console.WriteLine(item);
+            }
+            //문자열을 잘라야 하는 구분자가 2개 이상인 경우
+            //@랑 공백으로 문자열 자르고 싶을 때
+            string example = "ldj good@19 89@12 34";
+            string[] myinput = example.Split(new char[] { '@', ' ' });
+            //Join은 문자열 배열같은 걸 다시 뭉쳐준다.
+            //ldj good 19 89 12 34
+            //ldj,good,19,89,12,34
+            string total = string.Join(",", myinput);
+            foreach (var item in myinput)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine(total);
+
+            //,가 전부 없어지게 됨
+            string replace_test = total.Replace(",", "");
+            Console.WriteLine(replace_test);
+
         }
     }
 }
