afbf91bc53287460e8d3c251b96ea45e69e51222
DongJoonLeeDJ <ldjgood12@gmail.com>
2022-01-05 10:45:06 +0900

    조건문 하는 중

---
+++ djlee/cSharp/20220105/MyCSharp_20220105/MyCSharp_20220105_2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCSharp_20220105_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int testnum = 100;
+            if(testnum > 50)
+            {
+                Console.WriteLine("testnum은 50을 넘는다.");
+            }
+
+            if(testnum < 0 )
+            {
+                Console.WriteLine("testnum은 음수");
+            }
+            else
+            {
+                Console.WriteLine("testnum은 양수");
+            }
+
+            if(testnum == 10)
+            {
+                Console.WriteLine("testnum은 10이다.");
+            }
+            else if(testnum == 100)
+            {
+                Console.WriteLine("testnum은 100이다.!!!");
+            }
+            else
+            {
+                Console.WriteLine($"testnum은 {testnum}이다.");
+            }
+        }
+    }
+}
