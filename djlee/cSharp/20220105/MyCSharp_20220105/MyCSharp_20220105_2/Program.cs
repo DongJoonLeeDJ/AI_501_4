12d5648f54a2511605c29d97ba63b912ba6ee9c5
DongJoonLeeDJ <ldjgood12@gmail.com>
2022-01-05 10:59:23 +0900

    문제1 풀어봄

--- djlee/cSharp/20220105/MyCSharp_20220105/MyCSharp_20220105_2/Program.cs
+++ djlee/cSharp/20220105/MyCSharp_20220105/MyCSharp_20220105_2/Program.cs
@@ -37,6 +37,27 @@ namespace MyCSharp_20220105_2
             {
                 Console.WriteLine($"testnum은 {testnum}이다.");
             }
+
+
+            Console.WriteLine("나이를 입력해주세요.");
+            int age = int.Parse(Console.ReadLine());
+            if(age < 0 || age > 123 )
+                Console.WriteLine("타노스");
+            else if(age>=0 && age<20)
+                Console.WriteLine("미성년자");
+            else if(age<40)//else if(age>=20 && age <40)
+                Console.WriteLine("청년");
+            else if(age<60)
+                Console.WriteLine("중년");
+            else if(age<100)
+                Console.WriteLine("노년");
+            else if(age<=123)
+                Console.WriteLine("장수");
+            //else
+            //{
+
+            //}
+
         }
     }
 }
