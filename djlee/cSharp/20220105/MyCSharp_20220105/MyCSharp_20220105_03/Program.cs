bd40d67558411493b5bc6a907b766f735ecd3f05
DongJoonLeeDJ <ldjgood12@gmail.com>
2022-01-05 11:45:24 +0900

    switch 문 및 삼항연산자 그리고 문제

---
+++ djlee/cSharp/20220105/MyCSharp_20220105/MyCSharp_20220105_03/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCSharp_20220105_03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //region 영역
+            #region MyIfStatementTest
+            //alt + 방향키(위,아래) : 코드 이동함
+            bool flag = true;
+            /*<-눌러*/
+            if (flag) //flag 값을 false로 두면 이 중괄호 안에 있는 것들 다 실행x
+            {
+                //switch문
+                char hakjeom = 'A';
+                switch (hakjeom)
+                {
+                    case 'A'://case 밑에 뭔가 한 줄이라도 있으면 break로 끝내야 함
+                        Console.WriteLine("엘리트");
+                        break;
+                    case 'B':
+                        Console.WriteLine("나쁘진않음");
+                        break;
+                    case 'C': //case 밑에 하나도 없는 경우에만 이렇게 쓸 수 있음
+                    case 'D':
+                        Console.WriteLine("좀 문제있음");
+                        break;
+                    case 'F':
+                        Console.WriteLine("ㅠㅠ");
+                        break;
+                    default:
+                        Console.WriteLine($"{hakjeom}은 유효하지 않습니다.");
+                        break;
+                }
+                //삼항 연산자
+                //if나 switch문보다 간단하게 쓸 수 있는 연산자
+                //근데 if나 switch에 익숙한 사람이 보면 조금 난해할 수 있음
+                //나는 안 쓰더라도 이걸 쓰는 사람은 꼭 1명쯤은 있다.
+
+                int mynumber = 100;
+                //mynumber를 2로 나눠서 0인 경우(=짝수)
+                //result 변수에 "짝수" 대입
+                //그게 아닌 경우(=홀수) "홀수" 대입
+
+                //(참거짓을판별할식) ? (그 식이 참인 경우) : (그 식이 거짓인 경우)
+                string result = mynumber % 2 == 0 ? "짝수" : "홀수";
+                Console.WriteLine(result);
+                Console.WriteLine(mynumber % 2 == 0 ? "짝수" : "홀수");
+
+                string Hello = "Hello World";
+                bool t1 = Hello.Contains("Hell");
+                bool t2 = Hello.Contains("HELL");
+                Console.WriteLine("t1=" + t1);
+                Console.WriteLine("t2=" + t2);
+
+            }
+            #endregion
+        }
+    }
+}
