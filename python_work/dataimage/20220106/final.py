ec214d8c6d413bec649ad51e307ffb695f1e387c
DongJoonLeeDJ <ldjgood12@gmail.com>
2022-01-10 15:20:23 +0900

    0110
    0110

---
+++ python_work/dataimage/20220106/final.py
@@ -0,0 +1,27 @@
+
+from selenium import webdriver  #크롬브라우저
+import urllib.request   #파일다운로드
+import time
+import resultimg
+
+web = webdriver.Chrome()
+web.get('http://192.168.51.3:10000/')
+time.sleep(1)
+web.find_element('id','start').click()
+
+for i in range(1,31):
+    time.sleep(0.5)
+    img = web.find_element('id','img'+str(i))
+    src = img.get_attribute('src')
+
+    name = './img/'+str(i)+'.png'
+    # download the image
+    urllib.request.urlretrieve(src,name)
+
+    ret = resultimg.get_result(str(i))
+    web.find_element('id','ans').send_keys(ret)
+    web.find_element('id','submit').click()
+
+time.sleep(5)
+web.quit()
+
--- python_work/dataimage/20220106/resultimg.py
+++ python_work/dataimage/20220106/resultimg.py
@@ -3,26 +3,34 @@
 import utils
 import cv2
 import Kncl
+import re
 
-chars = utils.extract_chars('3')
-'''2.png = eval(91+94)'''
+def get_result(filename):
+    try:
+        chars = utils.extract_chars(filename)
 
-result_string = ""
-for char in chars:
-    test = cv2.resize(char[1], (20, 20))
-    test = test.reshape(-1,400)
-    ret = Kncl.check(test)
+        result_string = ""
+        yon = ""
+        for char in chars:
+            test = cv2.resize(char[1], (20, 20))
+            test = test.reshape(-1,400)
+            ret = Kncl.check(test)
 
-    if int(ret) > -1 and int(ret)< 10:
-        result_string += str(ret[0])
-    if int(ret) == 10:
-        result_string += '+'
-    elif int(ret) == 11:
-        result_string += '-'
-    elif int(ret) == 12:
-        result_string += '*'
+            if int(ret) > -1 and int(ret)< 10:
+                result_string += str(ret[0])
+            if int(ret) == 10:
+                result_string += '+'
+                yon = "+"
+            elif int(ret) == 11:
+                result_string += '-'
+                yon = "-"
+            elif int(ret) == 12:
+                result_string += '*'
+                yon = "*"
+        temp = re.split('\+|\-|\*',result_string)
+        result_string = str(int(temp[0]))+yon+str(int(temp[1]))
+        print(result_string)
 
-print(result_string)
-# print(eval("111+222"))
-print(eval(result_string))
-
+        return eval(result_string)
+    except Exception as e:
+        print(e)
