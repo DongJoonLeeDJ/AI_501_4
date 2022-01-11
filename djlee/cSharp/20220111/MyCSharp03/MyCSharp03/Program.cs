2ba37694ea903951811d1bbf45114d3abc602c25
DongJoonLeeDJ <ldjgood12@gmail.com>
2022-01-11 11:01:25 +0900

    1번문제 풀고 2번문제(버블소트까지해봄)

---
+++ djlee/cSharp/20220111/MyCSharp03/MyCSharp03/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCSharp03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //1 입력시 1출력      - 0+1
+            //2~7입력시 2출력     - 6+1까지
+            //8~19입력시 3출력    - 12+7까지
+            //20~37 입력시 4출력  - 18+19까지
+            //36~61 입력시 5출력  - 24+37까지
+            Console.WriteLine("숫자를 입력하세요.");
+            int input = int.Parse(Console.ReadLine());
+            int max = 1; //각 벌집의 최댓값
+            int counter = 0; //임의의 카운트 변수
+            while (true)
+            {
+                max = max + (6 * counter);
+                if(max<input)
+                {
+                    counter++;
+                    continue;
+                }
+                else
+                    Console.WriteLine($"{input}은/는 {counter+1}번째 벌집에 속함");
+                break;
+            }
+        }
+    }
+}
---
+++ djlee/cSharp/20220111/MyCSharp03/MyCSharp03_algorithm/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCSharp03_algorithm
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("배열의 길이 정해주세요.");
+            //배열의 길이를 정해야 함
+            int arrsize = int.Parse(Console.ReadLine());
+
+            //배열 선언
+            int[] numbers = new int[arrsize];
+
+            for(int i = 0; i < arrsize; i++)
+            {
+                Console.WriteLine($"[{i}]번째 값 입력");//[0]번째값입력~ [arrsize-1]번째값 입력
+                numbers[i] = int.Parse(Console.ReadLine());
+            }
+
+            Array.Sort(numbers); //정렬(크기순 정렬)
+            //내림차순 하고 싶으면 아래줄 이 코드 주석만 해제하면 됨
+            //Array.Reverse(numbers); //내가 만든 배열의 인덱스를 거꾸로 뒤집는 거 (크기순 정렬 아님)
+
+            //그래서 만약 내림차순 정렬하고 싶으면 Sort를 통해서 오름차순 정렬 후, 그걸 Reverse하면 됨
+
+            foreach (var item in numbers)
+            {
+                Console.WriteLine(item);
+            }
+
+            Array.Reverse(numbers); // 오름차순정렬을 하기 위해서, 일부러 반대로 뒤집어봄...
+
+            for(int i = 0; i<arrsize;i++)
+            {
+                for(int j = 0; j<arrsize-1; j++)
+                {
+                    if(numbers[j] > numbers[j+1])
+                    {
+                        int temp = numbers[j];
+                        numbers[j] = numbers[j + 1];
+                        numbers[j + 1] = temp;
+                    }
+                }
+            }
+
+            foreach (var item in numbers)
+            {
+                Console.WriteLine(item);
+            }
+
+
+
+        }
+    }
+}
