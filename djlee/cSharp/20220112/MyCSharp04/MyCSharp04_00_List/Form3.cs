9f76f9ad81c1e3ac26246570c9c5799d44e23d8b
DongJoonLeeDJ <ldjgood12@gmail.com>
2022-01-12 14:33:43 +0900

    참조변수 개념 설명함

--- djlee/cSharp/20220112/MyCSharp04/MyCSharp04_00_List/Form3.cs
+++ djlee/cSharp/20220112/MyCSharp04/MyCSharp04_00_List/Form3.cs
@@ -16,5 +16,39 @@ namespace MyCSharp04_00_List
         {
             InitializeComponent();
         }
+
+        void plus(int a)
+        {
+            a++;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = 100;
+            plus(a);
+            textBox1.Text = a.ToString();
+        }
+
+        void plus(TextBox t)
+        {
+            t.Text = int.Parse(t.Text) + 1 + "";
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = 100+"";
+            plus(textBox2);
+        }
+
+        void plus(Product p)
+        {
+            p.price++;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Product p = new Product();
+            p.price = int.Parse(textBox3.Text);
+            plus(p);
+            textBox3.Text = p.price.ToString();
+        }
     }
 }
