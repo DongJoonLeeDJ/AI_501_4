30dec968d794e997b424cd291e333a3ee1a6218a
DongJoonLeeDJ <ldjgood12@gmail.com>
2022-01-12 12:40:41 +0900

    Update Form2.cs

--- djlee/cSharp/20220112/MyCSharp04/MyCSharp04_00_List/Form2.cs
+++ djlee/cSharp/20220112/MyCSharp04/MyCSharp04_00_List/Form2.cs
@@ -29,8 +29,52 @@ namespace MyCSharp04_00_List
             button4.Text = random.Next(100).ToString();
             button8.Text = button4.Text;
         }
+
+        void changeColor(Label l)
+        {
+            l.BackColor = Color.Transparent;
+            if (int.Parse(l.Text) <= 10)
+            {
+                l.ForeColor = Color.Yellow;
+                l.BackColor = Color.Black;
+            }
+            else if (int.Parse(l.Text) <= 20)
+                l.ForeColor = Color.Blue;
+            else if (int.Parse(l.Text) <= 30)
+                l.ForeColor = Color.Red;
+            else if (int.Parse(l.Text) <= 40)
+                l.ForeColor = Color.Black;
+            else
+                l.ForeColor = Color.Green;
+        }
+
+
         private void button9_Click(object sender, EventArgs e)
         {
+            //랜덤한 7개의 숫자를 뽑는다. 단 이 7개는 서로 중복되면 안 됨.
+            int[] lotto = new int[7];
+            //i<7 이렇게 적어도 되긴 함.
+            for (int i = 0; i < lotto.Length; i++)
+            {
+                //int num = new Random().Next(45) + 1;
+                Random random = new Random();
+                int num = random.Next(45) + 1;
+                if (lotto.Contains(num)) //이미 존재한다면... i값을 깎는다. 그러면 다음 단계에서 i가 원상복구됨
+                    i--;
+                else
+                    lotto[i] = num;
+            }
+            Array.Sort(lotto);
+
+            num1.Text = lotto[0].ToString();
+            num2.Text = lotto[1].ToString();
+            num3.Text = lotto[2].ToString();
+            num4.Text = lotto[3].ToString();
+            num5.Text = lotto[4].ToString();
+            num6.Text = lotto[5].ToString();
+            num7.Text = lotto[6].ToString();
+
+
 
         }
 
