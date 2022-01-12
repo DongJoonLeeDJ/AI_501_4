10d574c4b95461338df8b96988494922256201f8
DongJoonLeeDJ <ldjgood12@gmail.com>
2022-01-12 12:28:16 +0900

    list 완료

--- djlee/cSharp/20220112/MyCSharp04/MyCSharp04_00_List/Form2.cs
+++ djlee/cSharp/20220112/MyCSharp04/MyCSharp04_00_List/Form2.cs
@@ -18,13 +18,23 @@ namespace MyCSharp04_00_List
         public Form2()
         {
             InitializeComponent();
+
+            Random random = new Random();
+            button1.Text = random.Next(100).ToString();
+            button5.Text = button1.Text;
+            button2.Text = random.Next(100).ToString();
+            button6.Text = button2.Text;
+            button3.Text = random.Next(100).ToString();
+            button7.Text = button3.Text;
+            button4.Text = random.Next(100).ToString();
+            button8.Text = button4.Text;
         }
         private void button9_Click(object sender, EventArgs e)
         {
 
         }
 
-        void addText(string text)
+        void addText(string text) //list라는 stringList에 텍스트추가 하고 ListText(=label)에 글자적기
         {
             list.Add(text);
             ListText.Text = "";
@@ -33,7 +43,7 @@ namespace MyCSharp04_00_List
                 ListText.Text += item + " ";
             }
         }
-        void removeText(string text)
+        void removeText(string text)//list라는 stringList에 텍스트삭제 하고 ListText(=label)에 글자적기
         {
             list.Remove(text);
             ListText.Text = "";
@@ -45,42 +55,44 @@ namespace MyCSharp04_00_List
 
         private void button1_Click(object sender, EventArgs e)
         {
-
+            addText(button1.Text);
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
-
+            addText(((Button)sender).Text);
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-
+            //as 로써
+            //sender 변수(=object변수)를 Button으로 형변환
+            addText((sender as Button).Text);
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-
+            addText((sender as Button).Text);
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
-
+            removeText((sender as Button).Text);
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
-
+            removeText((sender as Button).Text);
         }
 
         private void button7_Click(object sender, EventArgs e)
         {
-
+            removeText((sender as Button).Text);
         }
 
         private void button8_Click(object sender, EventArgs e)
         {
-
+            removeText((sender as Button).Text);
         }
     }
 }
