feffeed6bf44ff16a4d22afb27c2da87cb929e3c
DongJoonLeeDJ <ldjgood12@gmail.com>
2022-01-12 09:25:12 +0900

    문제냈던거

--- djlee/cSharp/20220111/MyCSharp03/WindowsFormsAppStart/Form1.Designer.cs
+++ djlee/cSharp/20220111/MyCSharp03/WindowsFormsAppStart/Form1.Designer.cs
@@ -28,13 +28,133 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(34, 18);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.buttonbutton);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(135, 163);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(189, 163);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(247, 163);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 12);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(334, 163);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 12);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(413, 163);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 12);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(466, 163);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 12);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(541, 163);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(38, 12);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "label7";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(34, 122);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- djlee/cSharp/20220111/MyCSharp03/WindowsFormsAppStart/Form1.cs
+++ djlee/cSharp/20220111/MyCSharp03/WindowsFormsAppStart/Form1.cs
@@ -16,5 +16,76 @@ namespace WindowsFormsAppStart
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //label1, button1과 같은 것들을 컨트롤 혹은 컴포넌트라고 부름!
+            Random r = new Random();
+            label1.Text = r.Next(10).ToString();//0~9까지의 값 나옴
+        }
+
+        //sender : 이벤트를 호출시키는 주체.
+        //단 그 주체의 Text값등을 보고 싶다면 아래처럼 강제형변환 필요함
+        
+        //e : 이벤트에 대한 부가적인 정보인데, 프로그램 종료같은 이벤트가 아니면
+        //별 다른 게 없음
+        //FormClose 계열인 경우엔 종료원인등이 나옴
+        private void buttonbutton(object sender, EventArgs e)
+        {
+            
+            MessageBox.Show(e.ToString() + ((Button)sender).Text + "안녕하십니가!!!!!");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int[] lotto = new int[7];
+            for(int i = 0; i<7; i++)
+            {
+                int num = new Random().Next(45) + 1;
+                if (lotto.Contains(num) == false)
+                    lotto[i] = num;
+                else
+                    i--;
+            }
+            Array.Sort(lotto);
+            label1.Text = lotto[0].ToString();
+            label2.Text = lotto[1].ToString();
+            label3.Text = lotto[2].ToString();
+            label4.Text = lotto[3].ToString();
+            label5.Text = lotto[4].ToString();
+            label6.Text = lotto[5].ToString();
+            label7.Text = lotto[6].ToString();
+
+            // 궁금해하시는 분들을 위한 추가
+            /*
+             1번부터 10번까지는 노란색입니다.
+            11번 부터 20번까지는 파란색입니다.
+            21번부터 30번까지는 빨간색입니다.
+            31번부터 40번까지는 검은색입니다.
+            41번부터 45번까지는 초록색입니다.
+             */
+
+            Label[] labels = { label1, label2, label3, label4, label5, label6, label7 };
+            for (int i = 0; i < 7; i++)
+            {
+
+                labels[i].BackColor = Color.Transparent;
+                if (int.Parse(labels[i].Text) <= 10)
+                {
+                    labels[i].ForeColor = Color.Yellow;
+                    labels[i].BackColor = Color.Black;
+                }
+                else if (int.Parse(labels[i].Text) <= 20)
+                    labels[i].ForeColor = Color.Blue;
+                else if (int.Parse(labels[i].Text) <= 30)
+                    labels[i].ForeColor = Color.Red;
+                else if (int.Parse(labels[i].Text) <= 40)
+                    labels[i].ForeColor = Color.Black;
+                else if (int.Parse(labels[i].Text) <= 45)
+                    labels[i].ForeColor = Color.Green;
+
+            }
+
+        }
     }
 }
