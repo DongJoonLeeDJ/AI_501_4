6ff54890329ea24aa056ba913a63a59b40e8bbf1
DongJoonLeeDJ <ldjgood12@gmail.com>
2022-01-14 14:49:03 +0900

    0114
    0114

--- python_work/alone_ml_dl/chap4/ex01.py
+++ python_work/alone_ml_dl/chap4/ex01.py
@@ -48,9 +48,11 @@ for hip in range(1,6):
 
 # plt.plot(train_score)
 # plt.plot(test_score)
+# plt.xlabel('n_neighbors')
+# plt.ylabel('scores')
 # plt.show()
 
-knclf = KNeighborsClassifier(n_neighbors=3)
+knclf = KNeighborsClassifier(n_neighbors=5)
 knclf.fit(train_scaled,train_target)
 
 print(knclf.classes_)
@@ -59,5 +61,56 @@ print(np.round(proba,decimals=5))
 
 dis,indexes = knclf.kneighbors([test_scaled[3]])
 print(train_target[indexes])
+
+# z = np.arange(-5,5,0.1) # a*x + b*y + c*z = z
+# phi = 1 / (1+np.exp(-z)) # 시그모이드 함수..
+#
+# plt.plot(z,phi)
+# plt.xlabel('z')
+# plt.ylabel('phi')
+# plt.show()
+
+# 인덱싱, 슬라이싱 [:-1]
+# char_arr = np.array(['A', 'B', 'C', 'D', 'E'])
+# print(char_arr[[True, False, True, False, False]])
+#
+# ary = np.array([1,2,3,4,5])
+# print(ary[[True,False,True,True,False]])
+
+bream_smelt_indexes = (train_target=='Bream') | (train_target=='Smelt')
+# print(bream_smelt_indexes)
+
+train_bream_smelt = train_scaled[bream_smelt_indexes] # Bream과 Smelt 의 특성값 5개씩 있는 x 119??
+target_bream_smelt = train_target[bream_smelt_indexes] # Bream과 Smelt 의 특성값 5개씩 있는 x 40???
+
+print(train_bream_smelt.shape)
+print(target_bream_smelt.shape)
+
+from sklearn.linear_model import LogisticRegression
+
+# bream smelt 이진 분류..
+lr = LogisticRegression()
+lr.fit(train_bream_smelt,target_bream_smelt)
+
+predvalue = lr.predict(train_bream_smelt[:5])
+print(train_bream_smelt[:5])
+print(predvalue)
+
+predprovalue = lr.predict_proba(train_bream_smelt[:5])
+print(predprovalue)
+
+'''
+    학습기를 통해서 학습을 하고.. 그 예측값이 제일 정확한거를 찾는거를 개발하는 학문...
+'''
+print('차수 가중치','절편 바이어스')
+print(lr.coef_, lr.intercept_)
+
+z값들 = lr.decision_function(train_bream_smelt[:5])
+print(z값들[0],z값들[1],z값들[2])
+
+from scipy.special import expit
+
+print(expit(z값들))
+
 
 
