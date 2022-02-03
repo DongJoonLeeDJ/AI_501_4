b2d59db54919f9381493276a736ea07458911f89
DongJoonLeeDJ <ldjgood12@gmail.com>
2022-02-03 15:13:55 +0900

    0203
    0203

--- mldl_work/chap6-1/ex01.py
+++ mldl_work/chap6-1/ex01.py
@@ -14,14 +14,14 @@ apple = fruits[0:100].reshape(-1,10000)
 pineapple = fruits[100:200].reshape(-1,10000)
 banana = fruits[200:].reshape(-1,10000)
 
-print(apple.shape)
-print(np.mean(apple.mean(axis=1)))
+# print(apple.shape)
+# print(np.mean(apple.mean(axis=1)))
 
-print(pineapple.shape)
-print(pineapple.mean(axis=1))
+# print(pineapple.shape)
+# print(pineapple.mean(axis=1))
 
-print(banana.shape)
-print(banana.mean(axis=1))
+# print(banana.shape)
+# print(banana.mean(axis=1))
 
 # plt.hist(np.mean(apple,axis=1),alpha=0.8)
 # plt.hist(np.mean(pineapple,axis=1),alpha=0.8)
@@ -61,8 +61,22 @@ apple_mean = np.mean(apple, axis =0).reshape(100,100)
 pineapple_mean = np.mean(pineapple, axis =0).reshape(100,100)
 banana_mean = np.mean(banana, axis =0).reshape(100,100)
 
-_,axis = plt.subplots(1,3,figsize=(15,3))
-axis[0].imshow(apple_mean,cmap='gray_r')
-axis[1].imshow(pineapple_mean,cmap='gray_r')
-axis[2].imshow(banana_mean,cmap='gray_r')
+# _,axis = plt.subplots(1,3,figsize=(15,3))
+# axis[0].imshow(apple_mean,cmap='gray_r')
+# axis[1].imshow(pineapple_mean,cmap='gray_r')
+# axis[2].imshow(banana_mean,cmap='gray_r')
+# plt.show()
+
+abs_diff = np.abs(fruits - banana_mean)
+abs_mean = np.mean(abs_diff,axis=(1,2))
+print(abs_mean)
+print(abs_mean.shape)
+
+apple_index = np.argsort(abs_mean)[:100]
+fig,axis = plt.subplots(10,10,figsize=(10,10))
+for i in range(10):
+    for j in range(10):
+        axis[i,j].imshow(fruits[apple_index[i*10+j]],cmap='gray_r')
+        axis[i,j].axis('off')
 plt.show()
+
