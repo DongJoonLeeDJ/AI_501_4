0234a355e0c5df7f2fb89f798e85478deae1a35f
DongJoonLeeDJ <ldjgood12@gmail.com>
2022-02-08 09:59:29 +0900

    챕6진행중

---
+++ djlee/cSharp/20220208/MyCSharp_from_06/MyCSharp_from_06/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyCSharp_from_06
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show(textBox1.Text);
+            Student student = new Student();
+            student.Name = textBox1.Text;
+            student.introduceSelf();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Student student = new Student();
+            student.Name = textBox2.Text;
+            student.introduceSelf();
+        }
+
+        private void textBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+                button1.PerformClick();
+        }
+
+        private void textBox2_KeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+                button2.PerformClick();
+        }
+    }
+}
---
+++ djlee/cSharp/20220208/MyCSharp_from_06/MyCSharp_from_06/Student.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCSharp_from_06
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public void introduceSelf()
+        {
+            Console.WriteLine("내 이름은 " + Name + "이다.");
+        }
+    }
+}
