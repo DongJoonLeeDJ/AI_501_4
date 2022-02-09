4e2ae987e3f2c892a886bc2a86603a188ff15466
DongJoonLeeDJ <ldjgood12@gmail.com>
2022-02-09 16:43:28 +0900

    챕터8 들어감

---
+++ djlee/cSharp/20220209/MyCSharp_from_08/MyCSharp_chap08_01_02/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyCSharp_chap08_01_02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int.TryParse(textBox1.Text, out int value1);
+            int.TryParse(textBox2.Text, out int value2);
+            MessageBox.Show("두 숫자의 합은 " + value1+value2+ "이다.");
+            MessageBox.Show("두 숫자의 합은 " + (value1+value2)+ "이다."); //괄호 안 하면 그냥 글자 더하기 됨
+        }
+    }
+}
