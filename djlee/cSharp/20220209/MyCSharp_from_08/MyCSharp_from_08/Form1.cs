d6a4949d8058508b9fe504780342013c26e6e633
DongJoonLeeDJ <ldjgood12@gmail.com>
2022-02-09 11:01:57 +0900

    연습문제 2개 풀었음

---
+++ djlee/cSharp/20220209/MyCSharp_from_08/MyCSharp_from_08/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyCSharp_from_08
+{
+    public partial class Form1 : Form
+    {
+        int answer = 0; //컴퓨터가 출력할 정답
+        int timerCount = 0; //timer가 Tick 할 때 마다 증가되는 숫자
+        public Form1()
+        {
+            InitializeComponent();
+            //answer = new Random().Next(10) + 1;
+            answer = new Random().Next(1, 11);//1이상 11미만
+            Console.WriteLine("정답 : " + answer);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int mychoice = int.Parse(textBox1.Text);
+            if(mychoice == answer)
+            {
+                MessageBox.Show("정답!");
+                answer = new Random().Next(1, 11);
+                Console.WriteLine("answer : " + answer);
+                label2.Text = "Finished!";
+                timerCount = 0; //정답 맞추고 나면 timerCount를 리셋시킴
+            }
+            else
+            {
+                MessageBox.Show("오답!");
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timerCount++;
+            //5초가 경과됨...
+            //if(timerCount %5 == 0) //5초 경과
+            if (timerCount >= 5)
+            {
+                label2.Text = "Timeout";
+            }
+            
+        }
+    }
+}
---
+++ djlee/cSharp/20220209/MyCSharp_from_08/answer2/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace answer2
+{
+    public partial class Form_MyGame : Form
+    {
+        int limitTimer = 0; //사용자가 설정하는 제한 시간
+        int answer = 0;
+        int nowtime = 0;
+        public Form_MyGame()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer_limit.Enabled = false;
+            nowtime = 0;
+
+            label2.Text = "Game Start!!!";
+            answer = new Random().Next(1, 26);//1이상 25이하. 
+            Console.WriteLine("answer : " + answer);
+
+            int count = 1; 
+            //버튼을 코드상에서 그려주는 부분
+            for(int i = 0; i<5; i++)
+            {
+                for(int j = 0; j<5; j++)
+                {
+                    Button button = new Button();
+                    Point point = new Point(); //컴포넌트들 위치값이 Point 타입이다. 참고로 이건 구조체로 만들어짐
+                    point.X = 115 * j;
+                    point.Y = 50 * i;
+                    button.Location = point;
+                    button.Click += Button_Click;
+                    button.Text = count.ToString();
+                    count++;
+                    Controls.Add(button); //Controls = Form1에 있는 모든 컴포넌트들을 의미함
+                }
+            }
+
+            timer_limit.Enabled = true;
+            try
+            {
+                nowtime = int.Parse(textBox1.Text);
+            }
+            catch (Exception ex)
+            {
+                nowtime = -1;
+            }
+        }
+
+        //sender = 이벤트를 발생시키는 주체
+        private void Button_Click(object sender, EventArgs e)
+        {
+            if( ((Button)sender).Text == answer.ToString()  )
+            {
+                label2.Text = "you win";
+                nowtime = 0;
+                timer_limit.Enabled = false;
+            }
+            else
+            {
+                label2.Text = "이거 보물 아님";
+            }
+        }
+
+        private void timer_limit_Tick(object sender, EventArgs e)
+        {
+            nowtime++;
+            //nowtime = -1 -> 시간제한을 잘못걸었을 때
+            if (nowtime >= limitTimer || nowtime == -1)
+            {
+                label2.Text = "Game Over!!";
+                nowtime = 0;
+                timer_limit.Enabled = false;
+            }
+        }
+    }
+}
