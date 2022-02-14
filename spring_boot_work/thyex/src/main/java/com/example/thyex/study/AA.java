32019a27c099352ceb8d9bcb52a2076316500851
DongJoonLeeDJ <ldjgood12@gmail.com>
2022-02-14 11:23:56 +0900

    0214
    0214

--- spring_boot_work/thyex/src/main/java/com/example/thyex/study/AA.java
+++ spring_boot_work/thyex/src/main/java/com/example/thyex/study/AA.java
@@ -1,22 +1,30 @@
 package com.example.thyex.study;
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Getter
 @Setter
 @ToString
+@NoArgsConstructor
 public class AA {
+
+    public AA(int a, String bb) {
+        this.a = a;
+        this.bb = bb;
+    }
+
+    public AA(Long id, int a, String bb) {
+        this.id = id;
+        this.a = a;
+        this.bb = bb;
+    }
 
     @Id
     @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     private int a;
--- spring_boot_work/thyex/src/main/java/com/example/thyex/study/AAController.java
+++ spring_boot_work/thyex/src/main/java/com/example/thyex/study/AAController.java
@@ -21,10 +21,34 @@ public class AAController {
     @Autowired
     AARepository aaRepository;
 
-    @GetMapping(value = "aa")
+    @GetMapping(value = "aaselect")
     public String select(){
         List<AA> list = aaRepository.findAll();
         System.out.println(list);
         return list.toString();
+    }
+
+    @GetMapping(value = "aainsert")
+    public String insert(){
+        aaRepository.save(new AA(11,"bbb"));
+        return "aa insert";
+    }
+
+    @GetMapping(value = "aaupdate")
+    public String update(){
+        aaRepository.save(new AA(1L,22,"ccc"));
+        return "aa update";
+    }
+
+    @GetMapping(value = "aadelete")
+    public String delete(){
+        aaRepository.delete(new AA(1L,22,"ccc"));
+        return "aa delete";
+    }
+
+    @GetMapping(value = "aadeleteall")
+    public String deleteall(){
+        aaRepository.deleteAll();
+        return "aa deleteAll";
     }
 }
