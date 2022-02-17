df9ac4a9a6f3753938456bddad6e690d6dae2147
DongJoonLeeDJ <ldjgood12@gmail.com>
2022-02-17 14:52:41 +0900

    0217
    0217

--- python_work/alone_ml_dl/naver/ex03.py
+++ python_work/alone_ml_dl/naver/ex03.py
@@ -5,44 +5,32 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 
 a = np.arange(-100,100,1)
-# b = np.arange(100,200,1)
 
-# train_input = np.column_stack([a,b])
-train_input = a
-train_target = a*3
-
-# train_input = np.asarray(train_input)
-# train_target = np.asarray(train_target)
-
-# print(train_input[:5])
-# print(train_target[:5])
-
-# xtrain,ytrain,xtarget,ytarget = train_test_split(train_input,train_target,random_state=42)
-
-# print(xtrain.shape)
-# print(xtarget.shape)
-
-# print(xtrain[:5])
-# print(xtarget[:5])
+train_input = a #[-100,-99,-98,-97,-96, ... 98,99]
+train_target = a*3 #[ -300, -297... 297]
 
 model = keras.Sequential()
-# model.add(keras.layers.Flatten(input_shape=(2,)))
-# model.add(keras.layers.Dense(1))
-model.add(keras.layers.Dense(units=1,activation='linear',input_dim=1))
+model.add(keras.layers.Dense(1,input_shape = (1,)))
 
 print(model.summary())
-model.compile(optimizer="adam",loss='mse',metrics=['mae'])
+model.compile(optimizer="adam", loss='mse') 
+# 경사하강법... 
+# 하이퍼 파라메타... adam... rmsprop....
+# 최소제곱법(mse) 회귀...
+# crossenropy 분류
 
-model.fit(train_input.reshape(-1,1),train_target,verbose=1,epochs=5)
-
-# print(model.weights)
+# 학습을 해라...
+# 조기 종료...
+model.fit(train_input.reshape(-1,1), train_target, verbose=1, epochs=900)
 
 predvalue = model.predict([10,12,20,30])
 print('predvalue')
 print(predvalue)
 
-lr = LinearRegression()
-lr.fit(train_input.reshape(-1,1),train_target)
+model.save('model-whole.h5')
 
-print('lrrr')
-print(lr.predict([[10],[12],[20],[30]]))
+# lr = LinearRegression()
+# lr.fit(train_input.reshape(-1,1),train_target)
+
+# print('lrrr')
+# print(lr.predict([[10],[12],[20],[30]]))
