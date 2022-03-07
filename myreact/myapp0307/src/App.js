d8dacfe455826851a2761b4afb098512ad63f744
DongJoonLeeDJ <ldjgood12@gmail.com>
2022-03-07 16:40:53 +0900

    0307
    0307

---
+++ myreact/myapp0307/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import MyDiv from './components/MyDiv';
+
+
+function App() {
+  return (
+    <div className="App">
+      <MyDiv a="aa" b="3"/>
+      <MyDiv a="bb" b="5"/>
+      <MyDiv a="cc" b="7"/>
+      <MyDiv a="dd" b="9"/>
+      <MyDiv a="ee" b="11"/>
+    </div>
+  );
+}
+
+export default App;
---
+++ myreact/myapp0307/src/components/MyDiv.js
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+
+const MyDiv = (props) => {
+    this.state({ });
+    return ( 
+        <div>
+            <h1>Mydiv {props.a}</h1>
+            <h5>{props.b}</h5>
+            <h3>{this.state.a}</h3>
+        </div> 
+    );
+}
+export default MyDiv;
