2069bece3b31e4d4da014e6ace0a6e46a8d8f87f
DongJoonLeeDJ <ldjgood12@gmail.com>
2022-03-18 16:28:38 +0900

    0318
    0318

--- myreact/ex0318/src/App.js
+++ myreact/ex0318/src/App.js
@@ -3,8 +3,24 @@ import TodoListTemplate from './components/TodoListTemplate.js'
 import Form from './components/Form.js'
 import TodoItemList from './components/TodoItemList';
 // import MyCom from './components/MyCom.js'
+import React, { useState } from 'react';
 
 function App() {
+  const [id,setId] = useState(3); // 이미 0,1,2 가 존재하므로 3으로 설정
+
+  const [input,setInput] = useState('')
+  const [todos,setTodos] = useState(
+    [
+      { id: 0, text: ' 리액트 소개', checked: false },
+      { id: 1, text: ' 리액트 소개', checked: true },
+      { id: 2, text: ' 리액트 소개', checked: false }
+    ]
+  )
+
+  const handleChange = (e)=>{
+    this.setState({ input : e.target.value} )
+  }
+
   return (
     <div>
       <TodoListTemplate form={<Form/>}>
