4d2514fc16dc18d081ca109948c3d8e9361d6bac
DongJoonLeeDJ <ldjgood12@gmail.com>
2022-03-18 17:07:28 +0900

    123
    123

--- myreact/ex0318/src/App.js
+++ myreact/ex0318/src/App.js
@@ -6,25 +6,43 @@ import TodoItemList from './components/TodoItemList';
 import React, { useState } from 'react';
 
 function App() {
-  const [id,setId] = useState(3); // 이미 0,1,2 가 존재하므로 3으로 설정
+  const [id,setId] = useState(3);
+  const [input,setInput] = useState('');
+  const [todos,setTodos] = useState([
+    { id: 0, text: ' 리액트 소개', checked: false },
+    { id: 1, text: ' 리액트 소개', checked: true },
+    { id: 2, text: ' 리액트 소개', checked: false }
+  ]);
 
-  const [input,setInput] = useState('')
-  const [todos,setTodos] = useState(
-    [
-      { id: 0, text: ' 리액트 소개', checked: false },
-      { id: 1, text: ' 리액트 소개', checked: true },
-      { id: 2, text: ' 리액트 소개', checked: false }
-    ]
-  )
-
+  const handleCreate = ()=>{
+    console.log('추가')
+    setId(id+1);
+    setTodos([...todos,
+      { id: id, text: input, checked: false }
+    ])
+    setInput('');
+    console.log(todos);
+  }
+  const handleKeyPress = (e) => {
+    // 눌려진 키가 Enter 면 handleCreate 호출
+    if(e.key === 'Enter') {
+      handleCreate();
+    }
+  }
   const handleChange = (e)=>{
-    this.setState({ input : e.target.value} )
+    console.log(e);
+    console.log(e.target.value);
+    setInput(e.target.value);
   }
 
   return (
     <div>
-      <TodoListTemplate form={<Form/>}>
-        <TodoItemList/>
+      <TodoListTemplate form={<Form 
+                  value={input} 
+                  onChange={handleChange}
+                  onKeyPress={handleKeyPress}
+                  onCreate={handleCreate}/>}>
+        <TodoItemList todos={todos}/>
       </TodoListTemplate>
     </div>
   );
